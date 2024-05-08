774809c41c34c81c98efd3d3baa20e2243229472
zwd <17326195691@163.com>
2024-05-08 16:07:56 +0800

    add todo list to use tauri

---
+++ todo_list/src/main.ts
@@ -0,0 +1,17 @@
+import "./assets/main.css";
+
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+import { invoke } from "@tauri-apps/api";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
+
+invoke("greet", { name: "zwd" }).then((response) => console.log(response));
