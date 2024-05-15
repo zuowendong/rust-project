0ce5cd432b2c6dbda9b818fe01ab4943ad4786c4
zwd <17326195691@163.com>
2024-05-15 16:44:41 +0800

    add basic

---
+++ data_type/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+  let food = "湖南小炒肉";
+  let price = 35;
+  let checked = true;
+
+  println!("food is {}", food);
+  println!("price is {}", price);
+  println!("checked is {}", checked);
+}
